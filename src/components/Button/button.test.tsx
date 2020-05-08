cbd109d242b6f266b55b952e5c0b45f5548c273b
Chenxiao Guan <1011zaozao@gmail.com>
2020-05-07 22:37:03 -0400

    test(button): added more test for button component

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import Button, { ButtonProps } from './button';
+import Button, { ButtonProps, ButtonType } from './button';
 
 describe('test Button Component', () => {
-  it('should render default button and listen to onClick event', () => {
+  it('should render default primary button and listen to onClick event', () => {
     const onClickMock = jest.fn();
     const propsWithOnClickMock: ButtonProps = {
       onClick: onClickMock,
+      btnType: ButtonType.Primary,
     };
     const buttonText = 'Default Button';
     const { getByText } = render(<Button {...propsWithOnClickMock}>{buttonText}</Button>);
@@ -15,5 +16,22 @@ describe('test Button Component', () => {
     expect(element).toBeInTheDocument();
     fireEvent.click(element);
     expect(onClickMock).toHaveBeenCalled();
+    expect(element.classList.contains('btn')).toBeTruthy();
+    expect(element.classList.contains('btn-primary')).toBeTruthy();
+  });
+
+  it('should render link button using anchor tag', () => {
+    const buttonText = 'Link Button';
+    const { getByText } = render(
+      <Button btnType={ButtonType.Link} href='https://google.com/'>
+        {buttonText}
+      </Button>,
+    );
+    const element = getByText(buttonText);
+
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe('a'.toUpperCase());
+    expect(element.classList.contains('btn')).toBeTruthy();
+    expect(element.classList.contains('btn-link')).toBeTruthy();
   });
 });
