68979e6a912c1b435bbec83ce9418a91dfaba421
Chenxiao Guan <1011zaozao@gmail.com>
2020-05-07 20:53:51 -0400

    test: modified test description
    modified the only test so that the description
    is more generic

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('should test true to be true', () => {
   render(<App />);
   expect(true).toBe(true);
 });
