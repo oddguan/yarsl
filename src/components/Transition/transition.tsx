e8242ba660d9fffe498e55c7c55e4ef1d4848d4a
Chenxiao Guan <1011zaozao@gmail.com>
2020-09-06 20:24:41 -0400

    fix(transition): added div wrapper option as prop
    the transition component sometimes doesn't work properly
    with components like button, so we need a wrapper optional
    prop to fix the unexpected behavior

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -6,13 +6,14 @@ export type AnimationName = 'zoom-in-top' | 'zoom-in-bottom' | 'zoom-in-right' |
 
 export type TransitionProps = CSSTransitionProps & {
   animation?: AnimationName;
+  wrapper?: boolean;
 };
 
 const Transition: React.FC<TransitionProps> = (props: React.PropsWithChildren<TransitionProps>): React.ReactElement => {
-  const { children, classNames, animation, ...restProps } = props;
+  const { children, classNames, animation, wrapper, ...restProps } = props;
   return (
     <CSSTransition classNames={classNames ? classNames : animation} {...restProps}>
-      {children}
+      {wrapper ? <div>{children}</div> : children}
     </CSSTransition>
   );
 };
